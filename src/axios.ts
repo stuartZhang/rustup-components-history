201bfc279dc1ab656048debcb6c75bb628240f03
stuart_zhang <stuartpage@qq.com>
2021-04-05 13:22:21 +0800

    请求失败日志

--- src/axios.ts
+++ src/axios.ts
@@ -3,27 +3,6 @@ import _ from 'underscore';
 import pcLog from 'mx-general.macros/dist/log-sync.macro';
 import {Message} from 'element-ui';
 
-
-class SessionLost{
-    public isLogout: boolean;
-    private promise?: Promise<void>;
-    static INSTANCE = new SessionLost();
-    constructor(){
-        this.isLogout = false;
-        this.promise = undefined;
-    }
-    action(force = false): Promise<void>{
-        if (this.isLogout || force) {
-            this.promise = this.promise || alertAsync('会话已登出').then(() => {
-                location.replace('/web_client/#/login');
-                return Promise.reject(new Error('Logout'));
-            });
-            return this.promise;
-        }
-        return Promise.resolve();
-    }
-}
-
 const TIME_OUT = 999999;
 const instance = axios.create({
     baseURL: process.env.baseURL,
@@ -36,7 +15,7 @@ function install(Vue: Vue.VueConstructor): void{
     });
 }
 
-instance.interceptors.request.use(config => SessionLost.INSTANCE.action().then(() => {
+instance.interceptors.request.use(config => {
     if (config.data instanceof URLSearchParams) {
         _.extendOwn(config.headers, {'Content-Type': 'application/x-www-form-urlencoded'});
         _.extendOwn(config, {data: config.data.toString()});
@@ -44,59 +23,28 @@ instance.interceptors.request.use(config => SessionLost.INSTANCE.action().then((
         _.defaults(config.headers, {'Content-Type': 'application/json;charset=UTF-8'});
     }
     return config;
-}), error => Promise.reject(error));
+}, error => Promise.reject(error));
 
-instance.interceptors.response.use(response => {
-    const {code, message, responseCode, msg, data} = response.data || {};
-    if (code != null && String(code) !== '0' && String(code) !== '200' ||
-        responseCode != null && String(responseCode) !== '80000') {
-        const errMsg = message || msg;
-        Message.error({
-            duration: 0,
-            showClose: true,
-            dangerouslyUseHTMLString: true,
-            message: errMsg ? errMsg.replace(/\n/ug, '<br>').replace(/\s+/gu, '&nbsp;') : '没有错误描述'
-        });
-        
-        pcLog.error('Axios 拦截到后端异常响应', response.config.method, response.config.url, response);
-        return Promise.reject(response);
-    }
-    if (data == null && /^get$/ui.test(response.config.method || '') &&
-        !Array.isArray(response.data) && typeof response.data === 'object') {
-        Message.error({
-            duration: 0,
-            showClose: true,
-            dangerouslyUseHTMLString: true,
-            message: `${response.config.method}-${response.config.url} 没有返回 data`
-        });
-        
-        pcLog.error(`Axios 拦截到后端异常响应 ${response.config.method}-${response.config.url} 没有返回 data`);
-        return Promise.reject(response);
-    }
-    return response;
-}, onAxiosErr);
+instance.interceptors.response.use(response => response, onAxiosErr);
 export function onAxiosErr(err: AxiosError): Promise<AxiosError | void>{
     pcLog.error(err);
+    let message = err.message || '请求失败';
     if (err.response) {
         const {data, status, statusText} = err.response;
-        if (~[401, 402].indexOf(status)) {
-            return SessionLost.INSTANCE.action(true);
-        }
-        let message;
         if (typeof data?.errors === 'object') {
             message = `[${data.errors.status_code}] ${data.errors.message}`;
         } else {
             message = `[${status}] ${statusText}`;
         }
-        Message.error({
-            duration: 0,
-            showClose: true,
-            dangerouslyUseHTMLString: true,
-            message
-        });
     } else if (err.code === 'ECONNABORTED') {
-        err.message = `连接超时，超过了 ${TIME_OUT / 1000} 秒`;
+        message = err.message = `连接超时，超过了 ${TIME_OUT / 1000} 秒`;
     }
+    Message.error({
+        duration: 0,
+        showClose: true,
+        dangerouslyUseHTMLString: true,
+        message
+    });
     return Promise.reject(err);
 }
 export {
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,29 +2,35 @@
     <el-container>
         <el-header class="search-bar">
             <h2 class="header"><code>rustup</code>组件历史状态表</h2>
-            <el-form inline :model="searchForm" class="search-form">
-                <el-form-item label="频道">
-                    <el-input v-model="searchForm.channel" placeholder="请输入内容" />
+            <el-form ref="searchFormRef" inline size="mini" :rules="searchForm.rules" :model="searchForm" class="search-form">
+                <el-form-item label="发布频道" prop="channel">
+                    <el-select v-model="searchForm.channel" allow-create filterable placeholder="请输入内容" class="channel-input">
+                        <el-option v-for="item in searchForm.channelCandidates" :key="item" :label="item" :value="item" />
+                    </el-select>
                 </el-form-item>
-                <el-form-item label="目标">
-                    <el-input v-model="searchForm.target" placeholder="请输入内容" />
+                <el-form-item label="目标平台" prop="target">
+                    <el-select v-model="searchForm.target" filterable placeholder="请选择" class="target-input">
+                        <el-option v-for="item in searchForm.targetCandidates" :key="item" :label="item" :value="item" />
+                    </el-select>
                 </el-form-item>
-                <el-form-item label="时间区间">
-                    <el-date-picker v-model="searchForm.dateRange" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" />
+                <el-form-item label="发布时间" prop="dateRange" class="date-range-input">
+                    <el-date-picker :clearable="false" v-model="searchForm.dateRange" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" />
                 </el-form-item>
-                <el-button type="primary" @click.prevent.stop="onClick">查询</el-button>
+                <el-form-item>
+                    <el-button type="primary" @click.prevent.stop="onClick">查询</el-button>
+                </el-form-item>
             </el-form>
         </el-header>
         <el-main class="search-result">
-            <el-table v-loading="searchResult.loading" height="100%" element-loading-text="拼命加载中" :data="searchResult.data" border>
-                <el-table-column fixed prop="name" label="组件" width="110" />
+            <el-table height="100%" v-loading="searchResult.loading" :element-loading-text="searchResult.progress" :data="searchResult.data" border>
+                <el-table-column fixed prop="name" label="组件" width="110" v-if="searchResult.data.length > 0" />
                 <el-table-column width="100" v-for="col of searchResult.columns" :key="col.label" :prop="col.label" :label="col.label" align="center" />
             </el-table>
         </el-main>
     </el-container>
 </template>
 <script lang="ts">
-import {defineComponent, reactive} from '@vue/composition-api';
+import {defineComponent, reactive, ref} from '@vue/composition-api';
 import pcLog from 'mx-general.macros/dist/log-sync.macro';
 import _ from 'underscore';
 export default defineComponent({
@@ -32,73 +38,117 @@ export default defineComponent({
     setup(props, context){ // eslint-disable-line max-lines-per-function
         const axios = context.parent.$axios;
         const moment = context.parent.$moment;
+        const searchFormRef = ref();
         const searchForm = reactive({
             dateRange: [moment('2020-11-01', 'YYYY-MM-DD').toDate(), moment('2020-11-30', 'YYYY-MM-DD').toDate()],
             channel: 'nightly',
-            target: 'x86_64-pc-windows-gnu'
+            channelCandidates: CHANNELS,
+            target: 'x86_64-pc-windows-gnu',
+            targetCandidates: TARGETS,
+            rules: {
+                channel: [{
+                    required: true,
+                    message: '频道不能是空',
+                    trigger: 'blur'
+                }, {
+                    trigger: 'blur',
+                    validator(rule, value, callback){
+                        if (~CHANNELS.indexOf(value) || /^(\d+\.)+(\d+)$/u.test(value)) {
+                            return callback();
+                        }
+                        return callback(new Error('频道格式不正确'));
+                    }
+                }],
+                target: [{
+                    required: true,
+                    message: '目标不能是空',
+                    trigger: 'blur'
+                }]
+            }
         });
         const searchResult = reactive({
+            progress: '',
             loading: false,
             columns: [],
             data: []
         });
         return {
-            searchResult,
+            searchFormRef,
             searchForm,
+            searchResult,
             async onClick(){
                 if (searchResult.loading) {
                     return;
                 }
-                searchResult.loading = true;
-                searchResult.data.length = 0;
-                searchResult.columns.length = 0;
-                const [startMom, endMom] = searchForm.dateRange.map(date => moment(date));
-                const days = [];
-                const components = new Set();
-                const promises = [];
-                while (startMom.isSameOrBefore(endMom)) {
-                    const date = startMom.format('YYYY-MM-DD');
-                    promises.push(requestSingleDay(date).then(dayItems => {
-                        const compItems = _.groupBy(dayItems, 'component');
-                        const compNames = Object.keys(compItems);
-                        compNames.forEach(compName => components.add(compName));
-                        days.push({
-                            date,
-                            compNames,
-                            dayItems
-                        });
-                    }));
-                    startMom.add(1, 'days');
-                }
-                await Promise.all(promises);
-                days.sort((a, b) => {
-                    const aa = moment(a.date, 'YYYY-MM-DD');
-                    const bb = moment(b.date, 'YYYY-MM-DD');
-                    if (aa.isBefore(bb)) {
-                        return -1;
-                    }
-                    if (aa.isAfter(bb)) {
-                        return 1;
-                    }
-                    return 0;
-                });
-                searchResult.columns = days.map(day => ({
-                    label: day.date
-                }));
-                components.forEach(compName => {
-                    const row = {
-                        name: compName
-                    };
-                    days.forEach(day => {
-                        if (~day.compNames.indexOf(compName)) {
-                            row[day.date] = 'Y';
+                await new Promise((resolve, reject) => {
+                    searchFormRef.value.validate(valid => {
+                        if (valid) {
+                            resolve();
                         } else {
-                            row[day.date] = '-';
+                            reject(new Error('表单验证失败'));
                         }
                     });
-                    searchResult.data.push(row);
                 });
-                searchResult.loading = false;
+                try {
+                    searchResult.loading = true;
+                    searchResult.data.length = searchResult.columns.length = 0;
+                    searchResult.progress = '拼命加载中';
+                    const [startMom, endMom] = searchForm.dateRange.map(date => moment(date));
+                    const totalDays = endMom.diff(startMom, 'days');
+                    let finisedDays = 0;
+                    const days = [];
+                    const components = new Set();
+                    const promises = [];
+                    while (startMom.isSameOrBefore(endMom)) {
+                        const date = startMom.format('YYYY-MM-DD');
+                        promises.push(requestSingleDay(date).then(dayItems => { // eslint-disable-line no-loop-func
+                            const compItems = _.groupBy(dayItems, 'component');
+                            const compNames = Object.keys(compItems);
+                            compNames.forEach(compName => components.add(compName));
+                            days.push({
+                                date,
+                                compNames,
+                                dayItems
+                            });
+                            finisedDays += 1;
+                            searchResult.progress = `拼命加载中，完成 ${Math.min(Math.floor(finisedDays / totalDays * 100), 98)}%`;
+                        }));
+                        startMom.add(1, 'days');
+                    }
+                    await Promise.all(promises);
+                    days.sort((a, b) => {
+                        const aa = moment(a.date, 'YYYY-MM-DD');
+                        const bb = moment(b.date, 'YYYY-MM-DD');
+                        if (aa.isBefore(bb)) {
+                            return -1;
+                        }
+                        if (aa.isAfter(bb)) {
+                            return 1;
+                        }
+                        return 0;
+                    });
+                    searchResult.columns = days.map(day => ({
+                        label: day.date
+                    }));
+                    components.forEach(compName => {
+                        const row = {
+                            name: compName
+                        };
+                        days.forEach(day => {
+                            if (~day.compNames.indexOf(compName)) {
+                                row[day.date] = 'Y';
+                            } else {
+                                row[day.date] = '-';
+                            }
+                        });
+                        searchResult.data.push(row);
+                    });
+                    searchResult.progress = '拼命加载中，完成 100%';
+                } finally {
+                    setTimeout(() => {
+                        searchResult.loading = false;
+                    }, 500);
+                }
             }
         };
         function requestSingleDay(date){
@@ -174,12 +224,25 @@ function renderFileSize(n){
         return Math.round(n * 100) / 100;
     }
 }
+const CHANNELS = ['stable', 'beta', 'nightly'];
+const TARGETS = ['aarch64-unknown-linux-gnu', 'i686-pc-windows-gnu', 'i686-pc-windows-msvc', 'i686-unknown-linux-gnu', 'x86_64-apple-darwin', 'x86_64-pc-windows-gnu', 'x86_64-pc-windows-msvc', 'x86_64-unknown-linux-gnu', 'aarch64-apple-darwin', 'aarch64-apple-ios', 'aarch64-fuchsia', 'aarch64-linux-android', 'aarch64-pc-windows-msvc', 'aarch64-unknown-linux-musl', 'arm-linux-androideabi', 'arm-unknown-linux-gnueabi', 'arm-unknown-linux-gnueabihf', 'arm-unknown-linux-musleabi', 'arm-unknown-linux-musleabihf', 'armv5te-unknown-linux-gnueabi', 'armv7-linux-androideabi', 'armv7-unknown-linux-gnueabihf', 'armv7-unknown-linux-musleabihf', 'asmjs-unknown-emscripten', 'i586-pc-windows-msvc', 'i586-unknown-linux-gnu', 'i586-unknown-linux-musl', 'i686-linux-android', 'i686-unknown-freebsd', 'i686-unknown-linux-musl', 'mips-unknown-linux-gnu', 'mips-unknown-linux-musl', 'mips64-unknown-linux-gnuabi64', 'mips64el-unknown-linux-gnuabi64', 'mipsel-unknown-linux-gnu', 'mipsel-unknown-linux-musl', 'powerpc-unknown-linux-gnu', 'powerpc64-unknown-linux-gnu', 'powerpc64le-unknown-linux-gnu', 's390x-unknown-linux-gnu', 'sparc64-unknown-linux-gnu', 'sparcv9-sun-solaris', 'wasm32-unknown-emscripten', 'wasm32-unknown-unknown', 'wasm32-wasi', 'x86_64-apple-ios', 'x86_64-fuchsia', 'x86_64-linux-android', 'x86_64-unknown-freebsd', 'x86_64-unknown-illumos', 'x86_64-unknown-linux-gnux32', 'x86_64-unknown-linux-musl', 'x86_64-unknown-netbsd', 'x86_64-unknown-redox', 'thumbv6m-none-eabi', 'thumbv7em-none-eabi', 'thumbv7em-none-eabihf', 'thumbv7m-none-eabi', 'aarch64-unknown-none', 'aarch64-unknown-none-softfloat', 'armebv7r-none-eabi', 'armebv7r-none-eabihf', 'armv5te-unknown-linux-musleabi', 'armv7-unknown-linux-gnueabi', 'armv7-unknown-linux-musleabi', 'armv7a-none-eabi', 'armv7r-none-eabi', 'armv7r-none-eabihf', 'mips64-unknown-linux-muslabi64', 'mips64el-unknown-linux-muslabi64', 'nvptx64-nvidia-cuda', 'riscv32i-unknown-none-elf', 'riscv32imac-unknown-none-elf', 'riscv32imc-unknown-none-elf', 'riscv64gc-unknown-linux-gnu', 'riscv64gc-unknown-none-elf', 'riscv64imac-unknown-none-elf', 'thumbv7neon-linux-androideabi', 'thumbv7neon-unknown-linux-gnueabihf', 'thumbv8m.base-none-eabi', 'thumbv8m.main-none-eabi', 'thumbv8m.main-none-eabihf', 'x86_64-fortanix-unknown-sgx', 'x86_64-pc-solaris'];
 </script>
 <style lang="scss" scoped>
 .search-bar {
     height: auto !important;
     .header {
         text-align: center;
+    }
+    .search-form {
+        .channel-input {
+            width: 100px;
+        }
+        .target-input {
+            width: 260px;
+        }
+        .date-range-input /deep/ .el-date-editor.el-range-editor.el-input__inner.el-date-editor--daterange {
+            width: 220px;
+        }
     }
 }
 .search-result {
