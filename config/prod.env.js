9b1c1a2d0c26f3cc2a450d0a06f2c323daaaf6b2
stuart_zhang <stuartpage@qq.com>
2021-04-05 11:12:24 +0800

    配置 sentry.io

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ module.exports = {
     MACRO_LOG_PRE_PROCESSOR: '"[__mx-log-macro/admin/rustup-components-history/pre-processor__]"',
     MACRO_LOG_TRANSFORMER: '"[__mx-log-macro/admin/rustup-components-history/transformer__]"',
     MACRO_LOG_POST_PROCESSOR: '"[__mx-log-macro/admin/rustup-components-history/post-processor__]"',
-    SENTRY_DSN: '"https://17250e8623f74c17997023086f228caf@o267132.ingest.sentry.io/1472860"',
+    SENTRY_DSN: '"https://6d559309450c4ccc82ac0c6f7a57879d@o267132.ingest.sentry.io/5705610"',
     SENTRY_ERR_TRACE: sentryErrTrace ? 'true' : 'false',
     RELEASE_VERSION: JSON.stringify(`rustup-components-history_${checknum.git.tagName}_${checknum.git.branchName}`),
     CHECKNUM: JSON.stringify(checknum),
